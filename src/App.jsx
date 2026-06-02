f3696fd515383de902b651b64ab0e81bdf42eaf2
aroamartindelarubia-coder <aroamartindelarubia@gmail.com>
2026-06-02 22:04:56 +0200

    agrego componentes a saas ia con React

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+//COMPONENTE 
+//1. Importamos la hoja de estilos App.css
+import './App.css';
+//2. Importaamos las dos grandes mitades de nuestra pantalla
+import Sidebar from './Sidebar';
+import ChatArea from './ChatArea';
+ 
 
 function App() {
   return (
-    <div>
-      <h1> ¡Hola React! Nuestro entorno está listo. 🚀</h1>
-    </div>
+    // Contenedor principal que usa flexbox (definido en el App.css)
+    //para poner las cosas lado a lado
+    <div className='app-container'> 
+    {/*Inyectamos la mitad izquierda a la pantalla*/}
+    <Sidebar/>
+    <ChatArea/>
+    
+      </div>
   )
 } 
 
-export default App 
+export default App;
