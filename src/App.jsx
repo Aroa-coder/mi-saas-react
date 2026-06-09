177276f66d6181956842efe149197c181275efab
aroamartindelarubia-coder <aroamartindelarubia@gmail.com>
2026-06-09 22:23:44 +0200

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-//COMPONENTE 
-//1. Importamos la hoja de estilos App.css
+// COMPONENTE PRINCIPAL: App
+// 1. Importamos la hoja de estilos App.css
 import './App.css';
-//2. Importaamos las dos grandes mitades de nuestra pantalla
+//2. Importamos las dos grandes mitades de nuestra pantañña
 import Sidebar from './Sidebar';
 import ChatArea from './ChatArea';
- 
 
 function App() {
   return (
-    // Contenedor principal que usa flexbox (definido en el App.css)
+    // Contenedor principal que usa flexbox(definido en el App.css)
     //para poner las cosas lado a lado
-    <div className='app-container'> 
-    {/*Inyectamos la mitad izquierda a la pantalla*/}
-    <Sidebar/>
-    <ChatArea/>
-    
-      </div>
+    <div className='app-container'>
+      {/*Inyectamos la mitad izquierda a la pantalla */}
+      <Sidebar />
+      {/* Inyectamos la mitad derecha de la pantalla */}
+     <ChatArea />
+    </div>
   )
-} 
+}
 
 export default App;
